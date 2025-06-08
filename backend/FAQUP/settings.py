abd0fd0b66ae22ea389e5ac6e9aa9ea6adc5f364
Lance <137799314+TiredCat388@users.noreply.github.com>
2025-06-08 23:47:08 +0800

    FINAL FIXED CORS PLS?

--- backend/FAQUP/settings.py
+++ backend/FAQUP/settings.py
@@ -118,7 +118,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://cmsc-126-final-exam.vercel.app/",
+    "https://cmsc-126-final-exam.vercel.app",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
